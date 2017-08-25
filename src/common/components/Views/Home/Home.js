3bbaa82e7d9ba689e54c901baf78192d4a5a6480
Jose Centeno <jmcenteno@gmail.com>
2017-08-24 22:43:46 -0400

    finished home page

--- src/common/components/Views/Home/Home.js
+++ src/common/components/Views/Home/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import { Page } from '../../Global';
@@ -8,7 +7,7 @@ import cow from './cow.png';
 const styles = {
   img: {
     width: '100%',
-    maxWidth: 300,
+    maxWidth: 250,
     margin: '24px auto 0'
   },
   h1: {
@@ -25,13 +24,16 @@ const styles = {
 export default class Home extends Component {
 
   render() {
-    console.log(this.props)
     return (
       <Page>
-        <div className='content' style={styles.content}>
+        <div className='content' style={ styles.content }>
           <div className='text-center'>
-            <img src={cow} className='img-responsive' style={styles.img} alt='cow' />
-            <h1 className='lead' style={styles.h1}>Welcome to BuyMyCow!</h1>
+            <img src={ cow } className='img-responsive' style={ styles.img } alt='cow' />
+            <h1 className='lead' style={ styles.h1 }>
+              Welcome to BuyMyCow!
+              <br />
+              <small>Where cows go moo.</small>
+            </h1>
             <br />
             <Link to='/products' className='btn btn-primary btn-lg'>Start Shopping</Link>
           </div>
