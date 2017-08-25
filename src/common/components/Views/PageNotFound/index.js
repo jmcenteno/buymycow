03892e9d4c46fb14a6d4ab5d189124e391e26777
Jose Centeno <jmcenteno@gmail.com>
2017-08-24 22:34:54 -0400

    404 page

--- src/common/components/Views/PageNotFound/index.js
+++ src/common/components/Views/PageNotFound/index.js
@@ -2,6 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
+import { Page } from '../../Global';
+import img from './404.png';
+
+const styles = {
+  content: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%'
+  },
+  img: {
+    width: 250,
+    marginBottom: 16
+  }
+}
+
 const PageNotFound = (props, context = {}) => {
 
   if (context.setStatus) {
@@ -9,16 +25,22 @@ const PageNotFound = (props, context = {}) => {
   }
   
   return (
-    <div>
-      <h1>Page not found</h1>
-      <Link to="/">Go home</Link>
-    </div>
+    <Page>
+      <div style={ styles.content }>
+        <div className='text-center'>
+          <img src={ img } alt='Page Not Found' style={ styles.img } />
+          <h1>Page Not Found</h1>
+          <br />
+          <Link to='/' className='btn btn-default'>Go Home</Link>
+        </div>
+      </div>
+    </Page>
   );
 
 }
 
 PageNotFound.contextTypes = {
-  setStatus: PropTypes.func.isRequired
+  setStatus: PropTypes.func
 }
 
 export default PageNotFound
