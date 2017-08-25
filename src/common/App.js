44e0c87fe54b055b004193c9d09ab44ada6cb1a2
Jose Centeno <jmcenteno@gmail.com>
2017-08-24 22:46:41 -0400

    set product status

--- src/common/App.js
+++ src/common/App.js
@@ -5,8 +5,11 @@ import firebase from 'firebase';
 import FirebaseConfig from './config/firebase';
 import Routes from './routes';
 import Layout from './components/Layout';
+import { checkProductStatus } from './cron';
 
 firebase.initializeApp(FirebaseConfig);
+
+checkProductStatus();
 
 const App = () => (
   <Layout>
