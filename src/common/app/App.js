af3abb065268e54df5ad85065727056a5c352b61
Jose Centeno <jmcenteno@gmail.com>
2017-10-29 13:20:01 -0400

    removed end date check

--- src/common/app/App.js
+++ src/common/app/App.js
@@ -5,11 +5,8 @@ import firebase from 'firebase';
 import FirebaseConfig from './config/firebase';
 import Routes from './routes';
 import Layout from './components/Layout';
-import { checkProductStatus } from './cron';
 
 firebase.initializeApp(FirebaseConfig);
-
-checkProductStatus();
 
 const App = () => (
   <Layout>
